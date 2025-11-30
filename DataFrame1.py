82a128dde8b875d6fc8e2787cfedde62abd6cbea
1707jai <jhind1546@gmail.com>
2025-11-30 09:35:08 +0000

    initial commit

---
+++ DataFrame1.py
@@ -0,0 +1,21 @@
+import pandas as pd
+data={
+    "Name":["jack","john","jully","jemi"],
+    "age":[24,23,22,21],
+    "city":["New York","Los Angeles","phoenix","San francisco"],
+    "salary":["30000","32000","30000","31500"]
+}
+
+df=pd.DataFrame(data)
+
+
+print(df)
+
+#row print
+print(df.loc[0])
+
+#add new data 
+
+df.loc[3]=["alex","25","Boston","25000"]
+
+print(df)
